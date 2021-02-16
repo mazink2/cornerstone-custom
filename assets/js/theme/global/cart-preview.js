18930b7ebb5e4c766edb864fd56a8fa9153791ff
mazin <mazin@instawalkin.com>
2021-02-15 23:06:24 -0500

    Added cart buttons (Add All To Cart and Remove All Items) to category page

--- assets/js/theme/global/cart-preview.js
+++ assets/js/theme/global/cart-preview.js
@@ -1,99 +1,133 @@
-import 'foundation-sites/js/foundation/foundation';
-import 'foundation-sites/js/foundation/foundation.dropdown';
-import utils from '@bigcommerce/stencil-utils';
+import "foundation-sites/js/foundation/foundation";
+import "foundation-sites/js/foundation/foundation.dropdown";
+import utils from "@bigcommerce/stencil-utils";
 
 export const CartPreviewEvents = {
-    close: 'closed.fndtn.dropdown',
-    open: 'opened.fndtn.dropdown',
+  close: "closed.fndtn.dropdown",
+  open: "opened.fndtn.dropdown",
+};
+
+// Helper function to show/hide "Remove All Items" button depending on quantity of cart
+export const toggleRemoveAllItemsBtn = (quantity = 0) => {
+  const $removeAllItemsBtn = $(".remove-all-items-btn");
+
+  if (quantity > 0) {
+    $removeAllItemsBtn.each(function (i, obj) {
+      if (obj.classList.contains("hide-btn")) {
+        obj.classList.remove("hide-btn");
+      }
+    });
+  } else {
+    $removeAllItemsBtn.each(function (i, obj) {
+      if (!obj.classList.contains("hide-btn")) {
+        obj.classList.add("hide-btn");
+      }
+    });
+  }
 };
 
 export default function (secureBaseUrl, cartId) {
-    const loadingClass = 'is-loading';
-    const $cart = $('[data-cart-preview]');
-    const $cartDropdown = $('#cart-preview-dropdown');
-    const $cartLoading = $('<div class="loadingOverlay"></div>');
+  const loadingClass = "is-loading";
+  const $cart = $("[data-cart-preview]");
+  const $cartDropdown = $("#cart-preview-dropdown");
+  const $cartLoading = $('<div class="loadingOverlay"></div>');
 
-    const $body = $('body');
+  const $body = $("body");
 
-    $body.on('cart-quantity-update', (event, quantity) => {
-        $cart.attr('aria-label', (_, prevValue) => prevValue.replace(/\d+/, quantity));
+  $body.on("cart-quantity-update", (event, quantity) => {
+    $cart.attr("aria-label", (_, prevValue) =>
+      prevValue.replace(/\d+/, quantity)
+    );
 
-        if (!quantity) {
-            $cart.addClass('navUser-item--cart__hidden-s');
-        } else {
-            $cart.removeClass('navUser-item--cart__hidden-s');
-        }
-
-        $('.cart-quantity')
-            .text(quantity)
-            .toggleClass('countPill--positive', quantity > 0);
-        if (utils.tools.storage.localStorageAvailable()) {
-            localStorage.setItem('cart-quantity', quantity);
-        }
-    });
-
-    $cart.on('click', event => {
-        const options = {
-            template: 'common/cart-preview',
-        };
-
-        // Redirect to full cart page
-        //
-        // https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
-        // In summary, we recommend looking for the string 'Mobi' anywhere in the User Agent to detect a mobile device.
-        if (/Mobi/i.test(navigator.userAgent)) {
-            return event.stopPropagation();
-        }
-
-        event.preventDefault();
-
-        $cartDropdown
-            .addClass(loadingClass)
-            .html($cartLoading);
-        $cartLoading
-            .show();
-
-        utils.api.cart.getContent(options, (err, response) => {
-            $cartDropdown
-                .removeClass(loadingClass)
-                .html(response);
-            $cartLoading
-                .hide();
-        });
-    });
-
-    let quantity = 0;
-
-    if (cartId) {
-        // Get existing quantity from localStorage if found
-        if (utils.tools.storage.localStorageAvailable()) {
-            if (localStorage.getItem('cart-quantity')) {
-                quantity = Number(localStorage.getItem('cart-quantity'));
-                $body.trigger('cart-quantity-update', quantity);
-            }
-        }
-
-        // Get updated cart quantity from the Cart API
-        const cartQtyPromise = new Promise((resolve, reject) => {
-            utils.api.cart.getCartQuantity({ baseUrl: secureBaseUrl, cartId }, (err, qty) => {
-                if (err) {
-                    // If this appears to be a 404 for the cart ID, set cart quantity to 0
-                    if (err === 'Not Found') {
-                        resolve(0);
-                    } else {
-                        reject(err);
-                    }
-                }
-                resolve(qty);
-            });
-        });
-
-        // If the Cart API gives us a different quantity number, update it
-        cartQtyPromise.then(qty => {
-            quantity = qty;
-            $body.trigger('cart-quantity-update', quantity);
-        });
+    if (!quantity) {
+      $cart.addClass("navUser-item--cart__hidden-s");
     } else {
-        $body.trigger('cart-quantity-update', quantity);
+      $cart.removeClass("navUser-item--cart__hidden-s");
     }
+
+    $(".cart-quantity")
+      .text(quantity)
+      .toggleClass("countPill--positive", quantity > 0);
+    if (utils.tools.storage.localStorageAvailable()) {
+      localStorage.setItem("cart-quantity", quantity);
+    }
+
+    // Show/hide "Remove All Items" button depending on quantity of cart
+    // Storefront API utilized here instead of utils.api.cart.getCartQuantity to satisfy task requirements
+    fetch("/api/storefront/cart", {
+      credentials: "include",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        toggleRemoveAllItemsBtn(data.length);
+      })
+      .catch((err) => {
+        toggleRemoveAllItemsBtn(0);
+        console.log(err);
+      });
+  });
+
+  $cart.on("click", (event) => {
+    const options = {
+      template: "common/cart-preview",
+    };
+
+    // Redirect to full cart page
+    //
+    // https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
+    // In summary, we recommend looking for the string 'Mobi' anywhere in the User Agent to detect a mobile device.
+    if (/Mobi/i.test(navigator.userAgent)) {
+      return event.stopPropagation();
+    }
+
+    event.preventDefault();
+
+    $cartDropdown.addClass(loadingClass).html($cartLoading);
+    $cartLoading.show();
+
+    utils.api.cart.getContent(options, (err, response) => {
+      $cartDropdown.removeClass(loadingClass).html(response);
+      $cartLoading.hide();
+    });
+  });
+
+  let quantity = 0;
+
+  if (cartId) {
+    // Get existing quantity from localStorage if found
+    if (utils.tools.storage.localStorageAvailable()) {
+      if (localStorage.getItem("cart-quantity")) {
+        quantity = Number(localStorage.getItem("cart-quantity"));
+        $body.trigger("cart-quantity-update", quantity);
+      }
+    }
+
+    // Get updated cart quantity from the Cart API
+    const cartQtyPromise = new Promise((resolve, reject) => {
+      utils.api.cart.getCartQuantity(
+        { baseUrl: secureBaseUrl, cartId },
+        (err, qty) => {
+          if (err) {
+            // If this appears to be a 404 for the cart ID, set cart quantity to 0
+            if (err === "Not Found") {
+              resolve(0);
+            } else {
+              reject(err);
+            }
+          }
+          resolve(qty);
+        }
+      );
+    });
+
+    // If the Cart API gives us a different quantity number, update it
+    cartQtyPromise.then((qty) => {
+      quantity = qty;
+      $body.trigger("cart-quantity-update", quantity);
+    });
+  } else {
+    $body.trigger("cart-quantity-update", quantity);
+  }
 }
